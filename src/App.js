d8c5eee34a6a2d807332eba32cbfb544a3974940
Lucas Teixeira <sobrinho1@gmail.com>
2020-10-08 18:05:17 -0300

    melhorias

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactSignatureCanvas from "react-signature-canvas";
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = React.useState({ trimmedDataURL: null });
+    let sigPad = {};
+    const clear = () => {
+        sigPad.clear();
+    };
+    const trim = () => {
+        setState({ trimmedDataURL: sigPad.getTrimmedCanvas().toDataURL("image/png") });
+        console.log(sigPad.getTrimmedCanvas().toDataURL("image/png"));
+    };
+    return (
+        <div className="App">
+            <ReactSignatureCanvas
+                backgroundColor="white"
+                penColor="black"
+                ref={(ref) => {
+                    sigPad = ref;
+                }}
+                canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+            />
+            <button onClick={clear}>Limpar</button>
+            <button onClick={trim}>Copiar Assinatura</button>
+            {state.trimmedDataURL ? <img alt="assinatura" src={state.trimmedDataURL} /> : null}
+        </div>
+    );
 }
 
 export default App;
